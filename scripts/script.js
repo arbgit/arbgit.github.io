ac4557c29a0fb7450c82d70d9822652c8d5d96c1
arbgit <edrf18@gmail.com>
2018-10-28 21:03:28 +0300

    update

---
+++ scripts/script.js
@@ -0,0 +1,67 @@
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var lowOrHi = document.querySelector('.lowOrHi');
+var guessSubmit = document.querySelector('.guessSubmit');
+var guessField = document.querySelector('.guessField');
+var guessCount = 1;
+var resetButton;
+
+function checkGuess() {
+  var userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Предыдущее значение: ';
+  }
+
+  guesses.textContent += userGuess + ' ';
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Поздравляем! Вы угадали число';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if(guessCount === 10) {
+    lastResult.textContent = '!!!ИГРА ОКОНЧЕНА!!!';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Неправивильно!';
+    lastResult.style.backgroundColor = 'red';
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = 'Значение меньше загаданного!';
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = 'Значение больше загаданного!';
+    }
+  }
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Начать новую игру';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+  var resultParas = document.querySelectorAll('.resultParas p');
+  for (var i = 0; i < resultParas.length; i++) {
+    resultParas[i].textContent = '';
+  }
+  resetButton.parentNode.removeChild(resetButton);
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField,focus();
+
+  lastResult.style.backgroundColor = 'white';
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
